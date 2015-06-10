f4faa0fe2866295460918d514ecf146f06d02fb5
Roberto <roberto@villavanilla.com>
2015-06-10 18:47:06 -0500

    Menú para seleccionar naves

--- Assets/Script/Managers/MenuController.cs
+++ Assets/Script/Managers/MenuController.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
 public class MenuController : MonoBehaviour {
 
 	public GameObject content;
+	public Image right;
+	public Image left;
+	public ScrollRect scrollRect;
+
+	protected Tween rT;
+	protected Tween lT;
+	protected bool checkScrollPosition = false;
+	protected bool rH = false;
+	protected bool lH = false;
+	protected float prev = -1;
 
 	void Start()
 	{
@@ -26,5 +37,58 @@ public class MenuController : MonoBehaviour {
 
 		GameObject lo = GameObject.Find("Level");
 		lo.GetComponent<Text>().text = "Nivel "+UserDataManager.instance.level;
+
+		left.DOFade(0,0.2f);
+	}
+
+	void Update()
+	{
+		if(checkScrollPosition)
+		{
+			if(prev == -1)
+			{
+				prev = scrollRect.velocity.x;
+				return;
+			}
+
+			if((int)prev*1000 != (int)scrollRect.velocity.x*1000)
+			{
+				prev = scrollRect.velocity.x;
+				return;
+			}
+
+			prev = -1;
+			checkScrollPosition = false;
+		
+			float val = ((int)(scrollRect.horizontalNormalizedPosition*1000))/1000.0f;
+
+			if(val <= 0)
+			{
+				//Solo derecha
+				right.DOFade(1,0.2f);
+			}
+			else if(val >= 1.0f)
+			{
+				//Solo izquierda
+				left.DOFade(1,0.2f);
+			}
+			else
+			{
+				left.DOFade(1,0.2f);
+				right.DOFade(1,0.2f);
+			}
+		}
+	}
+
+	public void onStartDrag()
+	{
+		right.DOFade(0,0.2f);
+		left.DOFade(0,0.2f);
+		checkScrollPosition = false;
+	}
+
+	public void onStopDrag()
+	{
+		checkScrollPosition = true;
 	}
 }
