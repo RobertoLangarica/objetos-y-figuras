cfe181ae2ae3d6a1295ba85d86a9aa69a8ba7370
Roberto <roberto@villavanilla.com>
2015-06-23 16:57:29 -0500

    -Quitando el hardcoding del menú principal (se agrego ScreenSelector)

---
+++ Assets/Script/ScenesMenu/ScreenSelector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSelector : MonoBehaviour {
+
+	public void selectNewScene(string sceneToGo)
+	{
+		ScreenManager.instance.GoToScene(sceneToGo);
+	}
+}
