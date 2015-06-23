d47e7688a8c2032532e67196a85a2f344143a417
Ricardo <ricardo@villavanilla.com>
2015-06-23 16:58:14 -0500

    escena soundShape nuevas figuras para el soundShape

---
+++ Assets/Script/SoundShape/SoundShapeManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundShapeManager : MonoBehaviour {
+
+	public static string shapeToPrepare = "Triangle";
+	public GameObject content;
+	public GameObject[] shapes;
+
+	protected int pieces = 4;
+	
+	protected int divs = 9;
+	protected int divsX = 3;
+	protected int divsY = 3;
+	// Use this for initialization
+	void Start () {
+
+		initializeShapes();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void initializeShapes()
+	{
+		GameObject go;
+		shapes = new GameObject[pieces];
+		float min;
+		float max;
+		Vector2[] pos = new Vector2[divs];
+		for(int j = 1, l =0;j < divsX+1;j++,l++)
+		{
+			for(int k = 1;k < divsY+1;k++)
+			{
+				pos[l] = new Vector2((Camera.main.aspect * Camera.main.orthographicSize)/(divsX*j),(Camera.main.orthographicSize)/(divsY*k));
+			}
+
+		}
+		for(int i = 0; i < pieces; i++)
+		{
+
+			GameObject shape = (GameObject)Resources.Load("SoundShapes/"+shapeToPrepare+i);
+			Debug.Log(pos[i] + "jnk");
+			go = GameObject.Instantiate(shape) as GameObject;
+			//shapes[i] = go;		
+			go.transform.SetParent(content.transform);
+			//go.transform.localPosition = new Vector3 (go.transform.localPosition.x,go.transform.localPosition.y,0);
+		}
+	}
+
+}
---
+++ Assets/Script/SoundShape/SoundShapeMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundShapeMenu : MonoBehaviour {
+	
+	public void Triangles()
+	{
+		SoundShapeManager.shapeToPrepare = "Triangle";
+		ScreenManager.instance.GoToScene ("SoundShapeGameplay");
+	}
+
+	public void Squares()
+	{
+		SoundShapeManager.shapeToPrepare = "Squares";
+		ScreenManager.instance.GoToScene ("SoundShapeGameplay");
+	}
+
+	public void Circles()
+	{
+		SoundShapeManager.shapeToPrepare = "Circles";
+		ScreenManager.instance.GoToScene ("SoundShapeGameplay");
+	}
+
+	public void Rhomboids()
+	{
+		SoundShapeManager.shapeToPrepare = "Rhomboids";
+		ScreenManager.instance.GoToScene ("SoundShapeGameplay");
+	}
+}
