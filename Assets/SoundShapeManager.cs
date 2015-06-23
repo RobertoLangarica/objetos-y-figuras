c6fbf4c96fd0fc25ca98cba455730ce55c2803c0
Ricardo <ricardo@villavanilla.com>
2015-06-23 18:22:29 -0500

    escena de triangulo para funcionar con sonidos dos sonidos de triangulo deshardcodeo de gamemenu

---
+++ Assets/SoundShapeManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundShapeManager : MonoBehaviour {
+
+	public AudioSource audioSource;
+	
+	public void selectSound(string soundToPlay)
+	{
+		AudioClip aC = (AudioClip)Resources.Load("Sounds/"+soundToPlay);
+		Debug.Log(aC);
+		audioSource.clip = aC;
+		audioSource.Play();
+	}
+}
