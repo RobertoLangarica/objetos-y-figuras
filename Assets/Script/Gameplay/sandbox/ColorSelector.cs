e846c452abdf93dfef47495ab798ebc78d8a2fac
Roberto <roberto@villavanilla.com>
2015-06-23 19:26:17 -0500

    -Prefabs y avances del menú y la instanciación para el sandbox de construcción

---
+++ Assets/Script/Gameplay/sandbox/ColorSelector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSelector : MonoBehaviour {
+
+	[HideInInspector]
+	public Color selectedColor;
+
+	void Awake()
+	{
+		selectedColor = new Color();
+	}
+
+	public void selectColor(int index)
+	{
+		switch(index)
+		{
+		case 1:
+			//RED
+			break;
+		case 2:
+			//GREEN
+			break;
+		case 3:
+			//BLUE
+			break;
+		case 4:
+			//PINK
+			break;
+		}
+	}
+}
---
+++ Assets/Script/Gameplay/sandbox/ShapeSelector.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeSelector : MonoBehaviour {
+
+	public GameObject square;
+	public GameObject rectangle;
+	public GameObject triangle;
+	public GameObject rhomboid;
+	public GameObject trapezium;
+
+	public ColorSelector colorSelector;
+
+
+	public void instantiateShape(string name)
+	{
+		switch(name)
+		{
+		case "square":
+			GameObject.Instantiate(square);
+			break;
+		case "rectangle":
+			GameObject.Instantiate(rectangle);
+			break;
+		case "triangle":
+			GameObject.Instantiate(triangle);
+			break;
+		case "rhomboid":
+			GameObject.Instantiate(rhomboid);
+			break;
+		case "trapezium":
+			GameObject.Instantiate(trapezium);
+			break;
+		}
+	}
+}
