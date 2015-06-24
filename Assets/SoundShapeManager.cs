ccae7517771c6a53256c025785acb2e28e094793
Ricardo <ricardo@villavanilla.com>
2015-06-23 19:08:29 -0500

    popUp de question escena de circulo rhomboide cuadradro

--- Assets/SoundShapeManager.cs
+++ Assets/SoundShapeManager.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SoundShapeManager : MonoBehaviour {
 
 	public AudioSource audioSource;
-	
+	public Text txt;
+	public GameObject popUp;
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			popUp.SetActive(false);
+		}
+	}
+
 	public void selectSound(string soundToPlay)
 	{
-		AudioClip aC = (AudioClip)Resources.Load("Sounds/"+soundToPlay);
-		Debug.Log(aC);
-		audioSource.clip = aC;
-		audioSource.Play();
+
+		#if TEACHER_MODE
+		//PopUp
+
+		#else
+			AudioClip aC = (AudioClip)Resources.Load("Sounds/"+soundToPlay);
+			Debug.Log(aC);
+			audioSource.clip = aC;
+			audioSource.Play();
+		#endif
+	}
+	public void question(string soundToPlay)
+	{
+		TextAsset tempTxt = (TextAsset)Resources.Load ("Levels/levels");
+		txt.text = tempTxt.text;
+		popUp.SetActive(true);
 	}
 }
