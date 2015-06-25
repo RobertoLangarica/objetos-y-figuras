77b909e5697329d235469868d903a229a05f055b
AliVilla <ali@villavanilla.com>
2015-06-24 19:55:45 -0500

    Subo lo de agrupar

---
+++ Assets/Script/GroupScene/GroupInput.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(DragRecognizer))]
+public class GroupInput : MonoBehaviour 
+{
+	protected GroupFigure selected;
+	protected Vector3 initialP = Vector3.zero;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<DragRecognizer> ().OnGesture += OnDrag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrag(DragGesture gesture)
+	{
+		switch(gesture.Phase)
+		{
+		case ContinuousGesturePhase.Started:
+			if(gesture.StartSelection)
+			{
+				selected = gesture.StartSelection.GetComponent<GroupFigure>();
+				selected.onTouchBegan(Camera.main.ScreenToWorldPoint(gesture.StartPosition));
+				initialP = Input.mousePosition;
+			}
+			break;
+			
+		case ContinuousGesturePhase.Updated:
+			if(selected && Input.mousePosition != initialP)
+			{
+				selected.onTouchMove(Camera.main.ScreenToWorldPoint(gesture.Position));
+				initialP = Input.mousePosition;
+			}
+			break;
+			
+		case ContinuousGesturePhase.Ended:
+			//selected.transform.localPosition=new Vector3(selected.transform.localPosition.x,selected.transform.localPosition.y,-0.1f);
+			
+			if(selected)
+			{
+				selected = null;
+			}
+			break;
+		}
+	}
+}
---
+++ Assets/Script/GroupScene/GroupScene.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GroupScene : MonoBehaviour 
+{
+	public enum EGroups
+	{
+		SHAPE,
+		COLOR,
+		SIZE
+	}
+
+	public static string typeOfGroup;
+
+	public int groupSize = 3;
+	public List<GameObject> kindsOfShapes = new List<GameObject>();
+
+	protected int totalGroups = 2;
+	public EGroups type;
+	protected GameObject[] currentShapes;
+
+	void Start()
+	{
+		currentShapes = new GameObject[groupSize*totalGroups];
+
+		generateShapes ();
+	}
+
+	protected void generateShapes()
+	{
+		List<int> ndxs = new List<int>();
+		for (int i = 0; i < kindsOfShapes.Count; i++) 
+		{
+			ndxs.Add(i);
+		}
+
+		switch (type) 
+		{
+		case(EGroups.SHAPE):
+		{
+			int rmdIdx = Random.Range(0,ndxs.Count);
+			ndxs.RemoveAt(rmdIdx);
+			int rmdIdx2 = Random.Range(0,ndxs.Count);
+			for(int i = 0;i < currentShapes.Length;i++)
+			{
+				if(i < groupSize)
+				{
+					currentShapes[i] = GameObject.Instantiate(kindsOfShapes[rmdIdx]) as GameObject;
+				}
+				else
+				{
+					currentShapes[i] = GameObject.Instantiate(kindsOfShapes[rmdIdx2]) as GameObject;
+				}
+			}
+		}
+			break;
+		case(EGroups.COLOR):
+		{
+			for(int i = 0;i < currentShapes.Length;i++)
+			{
+				currentShapes[i] = GameObject.Instantiate(kindsOfShapes[Random.Range(0,kindsOfShapes.Count)]) as GameObject;
+			}
+		}
+			break;
+		case(EGroups.SIZE):
+		{
+			GameObject rdm = kindsOfShapes[Random.Range(0,kindsOfShapes.Count)];
+			for(int i = 0;i < currentShapes.Length;i++)
+			{
+				currentShapes[i] = GameObject.Instantiate(rdm) as GameObject;
+			}
+		}
+			break;
+		}
+	}
+}
