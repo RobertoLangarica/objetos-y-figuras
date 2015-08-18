161fc4789b6a3c5a9c7ff5988bf2a0ef8af465ba
Roberto <roberto@villavanilla.com>
2015-08-18 14:43:07 -0500

    -Menu para 400Ordena y cada pantalla con botón de back

--- Assets/Script/ScenesMenu/ScreenSelector.cs
+++ Assets/Script/ScenesMenu/ScreenSelector.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ScreenSelector : MonoBehaviour {
-
+public class ScreenSelector : MonoBehaviour 
+{
 	public void selectNewScene(string sceneToGo)
 	{
 		ScreenManager.instance.GoToScene(sceneToGo);
