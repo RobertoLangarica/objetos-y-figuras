d7a587ff5caf89ec219bbeb49364e3c7dda88fe8
AliVilla <ali@villavanilla.com>
2015-08-17 19:24:33 -0500

    Tangrama con el shake transform, y las notificaciones

--- Assets/Script/Gameplay/Shape.cs
+++ Assets/Script/Gameplay/Shape.cs
@@ -19,8 +19,24 @@ public class Shape : BaseShape {
 	protected Vector3 positionDifference;//diferencia de posicon con el touch
 	[HideInInspector]
 	public SpriteRenderer sprite;
+	[HideInInspector]
+	public bool isPositionated = false;
 	protected float rot;
 	protected float mod;
+	protected float percent;
+	
+	//Para la destruccion
+	protected bool destroying = false;
+	protected Vector3 currentScale;
+	protected float inverseDestroyTime;
+	protected float destroyElapsed;
+	
+	
+	//Para el start
+	protected Vector3 initialScale;
+	protected bool starting;
+	protected float inverseStartTime;
+	protected float startElapsedTime;
 
 	// Use this for initialization
 	void Start () {
@@ -30,35 +46,76 @@ public class Shape : BaseShape {
 		boundaryRight = Camera.main.aspect * Camera.main.orthographicSize;
 
 		sprite = GetComponent<SpriteRenderer>();
+		
+		inverseStartTime = 1.0f/0.5f;
+		startElapsedTime = 0;
+		initialScale = transform.localScale;
+		transform.localScale = Vector3.zero;
+		currentScale = Vector3.zero;
+		currentScale.z = initialScale.z;
+		starting = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 centerDif = transform.position - transform.collider2D.bounds.center;
-		Vector3 nVec3 = transform.position;
-
-		if((transform.collider2D.bounds.center).x - (transform.collider2D.bounds.size.x * 0.5f) < boundaryLeft) 
+		if(starting)
 		{
-			nVec3.x = boundaryLeft + (transform.collider2D.bounds.size.x * 0.5f);
-			nVec3.x += centerDif.x;
+			percent = startElapsedTime*inverseStartTime;
+			currentScale.x = Mathf.SmoothStep(currentScale.x,initialScale.x,percent);
+			currentScale.y = Mathf.SmoothStep(currentScale.y,initialScale.y,percent);
+			
+			if(currentScale.x == initialScale.x && currentScale.y == initialScale.y)
+			{
+				starting = false;
+			}
+			
+			transform.localScale = currentScale;
+			startElapsedTime += Time.deltaTime;
 		}
-		if((transform.collider2D.bounds.center).x + (transform.collider2D.bounds.size.x * 0.5f) > boundaryRight) 
+		else if(destroying)
 		{
-			nVec3.x = boundaryRight - (transform.collider2D.bounds.size.x * 0.5f);
-			nVec3.x += centerDif.x;
+			percent = destroyElapsed*inverseDestroyTime;
+			currentScale.x = Mathf.SmoothStep(currentScale.x,0,percent);
+			currentScale.y = Mathf.SmoothStep(currentScale.y,0,percent);
+			
+			transform.localScale = currentScale;
+			
+			if(currentScale.x == 0 && currentScale.y == 0)
+			{
+				GameObject.DestroyImmediate(this.gameObject);
+			}
+			
+			destroyElapsed += Time.deltaTime;
 		}
-		if((transform.collider2D.bounds.center).y - (transform.collider2D.bounds.size.y * 0.5f) < boundaryBottom) 
+		
+		if(!destroying)
 		{
-			nVec3.y = boundaryBottom + (transform.collider2D.bounds.size.y * 0.5f);
-			nVec3.y += centerDif.y;
+			Vector3 centerDif = transform.position - transform.collider2D.bounds.center;
+			Vector3 nVec3 = transform.position;
+	
+			if((transform.collider2D.bounds.center).x - (transform.collider2D.bounds.size.x * 0.5f) < boundaryLeft) 
+			{
+				nVec3.x = boundaryLeft + (transform.collider2D.bounds.size.x * 0.5f);
+				nVec3.x += centerDif.x;
+			}
+			if((transform.collider2D.bounds.center).x + (transform.collider2D.bounds.size.x * 0.5f) > boundaryRight) 
+			{
+				nVec3.x = boundaryRight - (transform.collider2D.bounds.size.x * 0.5f);
+				nVec3.x += centerDif.x;
+			}
+			if((transform.collider2D.bounds.center).y - (transform.collider2D.bounds.size.y * 0.5f) < boundaryBottom) 
+			{
+				nVec3.y = boundaryBottom + (transform.collider2D.bounds.size.y * 0.5f);
+				nVec3.y += centerDif.y;
+			}
+			if((transform.collider2D.bounds.center).y + (transform.collider2D.bounds.size.y * 0.5f) > boundaryTop) 
+			{
+				nVec3.y = boundaryTop - (transform.collider2D.bounds.size.y * 0.5f);
+				nVec3.y += centerDif.y;
+			}
+			transform.position = nVec3;
 		}
-		if((transform.collider2D.bounds.center).y + (transform.collider2D.bounds.size.y * 0.5f) > boundaryTop) 
-		{
-			nVec3.y = boundaryTop - (transform.collider2D.bounds.size.y * 0.5f);
-			nVec3.y += centerDif.y;
-		}
-		transform.position = nVec3;
 	}
 
 	public void onRotationComplete()
@@ -81,4 +138,12 @@ public class Shape : BaseShape {
 
 		currentRotation = rot;
 	}	
+	
+	public void destroy(float delay)
+	{
+		currentScale = transform.localScale;
+		inverseDestroyTime = 1.0f/delay;
+		destroyElapsed = 0;
+		destroying = true;
+	}
 }
