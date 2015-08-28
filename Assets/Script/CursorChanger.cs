a54760f9c872fc1e07a854b0abde1cdf158fa6e9
Ricardo <ricardo@villavanilla.com>
2015-08-28 13:40:33 -0500

    pequeño ajuste pantallas de conoce y game menu

--- Assets/Script/CursorChanger.cs
+++ Assets/Script/CursorChanger.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class CursorChanger : MonoBehaviour {
@@ -14,6 +17,10 @@ public class CursorChanger : MonoBehaviour {
 	public Vector2 hotSpot = Vector2.zero;
 
 	public bool bPencil;
+
+	
+	EventTrigger eventTrigger = null;
+
 	void Awake()
 	{
 		instance = this;
@@ -67,5 +74,44 @@ public class CursorChanger : MonoBehaviour {
 		{
 			//Cursor.SetCursor(texture, hotSpot, cursorMode);
 		}
+	}
+
+	protected void searchButtons()
+	{
+		GameObject[] buttons;
+		buttons = GameObject.FindGameObjectsWithTag("UIButton");
+
+		foreach(GameObject button in buttons)
+		{
+			eventTrigger = button.AddComponent<EventTrigger>();
+			//button.GetComponent<Button>().OnPointerEnter.AddListener(() => { overButton(); });
+			//button.GetComponent<Button>().OnPointerExit.AddListener(() => { upChange(); }); 
+			//button.GetComponent<Button>().OnPointerDown.AddListener(() => { downButton(); }); 
+			//button.GetComponent<Button>().onClick.AddListener(() => { overButton(); }); 
+			eventTrigger = button.GetComponent<EventTrigger>();
+
+			Debug.Log(eventTrigger);
+
+			AddEventTrigger(ereaser, EventTriggerType.PointerEnter);
+		}
+	}
+
+	private void AddEventTrigger(UnityAction action, EventTriggerType triggerType)
+	{
+		// Create a nee TriggerEvent and add a listener
+		EventTrigger.TriggerEvent trigger = new EventTrigger.TriggerEvent();
+
+		trigger.AddListener((eventData) => action()); // you can capture and pass the event data to the listener
+
+		// Create and initialise EventTrigger.Entry using the created TriggerEvent
+		EventTrigger.Entry entry = new EventTrigger.Entry() { callback = trigger, eventID = triggerType };
+
+		// Add the EventTrigger.Entry to delegates list on the EventTrigger
+		eventTrigger.delegates.Add(entry);
+	}
+
+	void OnLevelWasLoaded()
+	{
+		searchButtons();
 	}
 }
