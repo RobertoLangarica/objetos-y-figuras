fa7b4142e9b990d08fe91bb6a9152847ca42563d
Roberto <roberto@villavanilla.com>
2015-09-02 21:23:09 -0500

    -Validator se agrego a todas las pantallas

--- Assets/Script/VV_Protection/VV_GameProtection.cs
+++ Assets/Script/VV_Protection/VV_GameProtection.cs
@@ -6,13 +6,9 @@ using System.IO;
 public class VV_GameProtection : MonoBehaviour 
 {
 
-	// Use this for initialization
 	void Start () 
 	{
 		TextAsset asset = Resources.Load("OYFVV2015_KIV") as TextAsset;
 		StreamReader reader = new StreamReader(asset.text);
-
-		Stream s = new MemoryStream(asset.bytes);
-		BinaryReader br = new BinaryReader(s);
 	}
 }
