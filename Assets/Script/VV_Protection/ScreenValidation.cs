b08906ab632d6bcdd86b77dc5c59efd0b737c7ac
Roberto <roberto@villavanilla.com>
2015-09-04 13:51:38 -0500

    -Avances

---
+++ Assets/Script/VV_Protection/ScreenValidation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenValidation : MonoBehaviour {
+
+	public Button acceptBtn;
+	public Image valid;
+	public Image invalid;
+	public InputField serialInput;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
