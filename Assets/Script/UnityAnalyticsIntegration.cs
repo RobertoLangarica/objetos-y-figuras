db582b667ec59e3a9996bb1543b9701b82cb0709
Ricardo Alvarez Gonzalez <ricardo@villavanilla.com>
2015-10-13 14:33:16 -0500

    analytics

--- Assets/Script/UnityAnalyticsIntegration.cs
+++ Assets/Script/UnityAnalyticsIntegration.cs
@@ -8,7 +8,7 @@ public class UnityAnalyticsIntegration : MonoBehaviour {
 	void Start () {
 		
 		const string projectId = "38870289-80d2-4270-800f-c1b4e2841ffd";
-		UnityAnalytics.StartSDK (projectId);
+		//UnityAnalytics.StartSDK (projectId);
 		
 	}
 	
